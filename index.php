7f0f7d88a00ab2fbf071b8ae4aebe8c6a77e6a3d
FaroukDourmane <56443340+FaroukDourmane@users.noreply.github.com>
2020-08-11 17:46:53 +0300

    update

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="ar" dir="rtl">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!--===============================================================================================-->
+     <link rel="icon" type="image/png" href="assets/img/ico.png"/>
+  <!--===============================================================================================-->
+  <!--===============================================================================================-->
+  <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@300;400;700&display=swap" rel="stylesheet">
+  <!--===============================================================================================-->
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="assets/css/main.css">
+    <title> مدارس رواد النجاح الأهلية </title>
+  </head>
+
+  <body>
+    <div class="header-container">
+      <div class="top">
+        <ul>
+          <li> <a href="#"> <img src="assets/svg/menu_toggle.svg" /> </a> </li>
+          <li> <a href="#">الرئيسية</a> </li>
+          <li> <a href="#">إتصل بنا</a> </li>
+          <li> <a href="#">من نحن</a> </li>
+          <li> <a href="#">بوابة التسجيل</a> </li>
+        </ul>
+
+        <div class="number">
+          ٠٥٩٨٥٠١٥١٥
+          <img src="assets/svg/phone.svg" />
+        </div>
+      </div>
+
+      <div class="content">
+        <img src="assets/img/logo.png" class="logo" />
+        <h1>مدارس رواد النجاح ترحب بكم</h1>
+        <a href="#" class="btn main">بوابة التسجيل</a>
+      </div>
+    </div>
+  </body>
+
+  <footer>
+    <script type="text/javascript" src="assets/js/jquery.min.js"></script>
+    <script type="text/javascript" src="assets/js/main.js"></script>
+  </footer>
+</html>
